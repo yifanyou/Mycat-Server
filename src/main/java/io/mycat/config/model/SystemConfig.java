05703667adcade58b6fbf4861595eb70a955c3fa
Billy <yyfan2016@gmail.com>
2016-10-30 10:59:08 +0800

    test

--- src/main/java/io/mycat/config/model/SystemConfig.java
+++ src/main/java/io/mycat/config/model/SystemConfig.java
@@ -34,7 +34,7 @@ import io.mycat.config.Isolations;
  * @author mycat
  */
 public final class SystemConfig {
-
+ 
 	public static final String SYS_HOME = "MYCAT_HOME";
 	private static final int DEFAULT_PORT = 8066;
 	private static final int DEFAULT_MANAGER_PORT = 9066;
